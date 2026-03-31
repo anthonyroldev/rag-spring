2b2c6a2f05fc0e7cf58acc22db1c1495eb029a88
AnthonyR <anthony156gamer@gmail.com>
2026-03-31 13:04:46 +0200

    refactor: clean code

--- src/main/java/com/anthonyroldev/ragapi/entities/DocumentChunk.java
+++ src/main/java/com/anthonyroldev/ragapi/entities/DocumentChunk.java
@@ -3,7 +3,6 @@ package com.anthonyroldev.ragapi.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.JdbcType;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
